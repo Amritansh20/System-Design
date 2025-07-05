c7d10eb5478126d7199267cd6ae0b9aed50bb104
Amritansh Soni <amritansh@Amritanshs-MacBook-Air.local>
2025-07-05 18:14:26 +0530

    Leaky Bucket Rate Limiter

--- src/main/java/org/systemDesign/rateLimiter/algorithms/LeakyBucket.java
+++ src/main/java/org/systemDesign/rateLimiter/algorithms/LeakyBucket.java
@@ -1,4 +1,56 @@
 package org.systemDesign.rateLimiter.algorithms;
 
+
+/*Imagine a bucket with a small hole in the bottom:
+
+        - Requests enter the bucket like water.
+        - Water leaks out at a constant rate.
+        - If the bucket overflows → request is rejected (rate limit hit)
+
+
+        - Fixed output rate: handles traffic evenly.
+        - No burst allowed (unlike Token Bucket).
+        - Smoother & fair for downstream systems.
+        - There is uniformity unlike Token bucket
+
+
+         Common Use Cases:
+
+        - Throttling user requests (prevent spamming)
+        - Rate-limiting login attempts
+        - Limiting API calls per service
+ */
 public class LeakyBucket {
+    private final int capacity;
+    private final double leakRatePerSecond;
+    private double currentWaterLevel;
+    private long lastLeakTimeStamp;
+
+    public LeakyBucket(int capacity, double leakRatePerSecond){
+        this.capacity=capacity;
+        this.leakRatePerSecond=leakRatePerSecond;
+        this.currentWaterLevel=capacity;
+        this.lastLeakTimeStamp=System.nanoTime();
+    }
+
+    public synchronized boolean allowRequests(){
+        leak();
+
+        if(currentWaterLevel<capacity){
+            currentWaterLevel++;
+            return true;
+        }
+        return false;
+    }
+
+    public void leak(){
+        long now = System.nanoTime();
+        double elapsedSeconds = (now-lastLeakTimeStamp)/1_000_000_000.0;
+
+        double leaked = elapsedSeconds*leakRatePerSecond;
+        if(leaked>0){
+            currentWaterLevel=Math.min(capacity,currentWaterLevel-leaked);
+            lastLeakTimeStamp= System.nanoTime();
+        }
+    }
 }
--- src/main/java/org/systemDesign/rateLimiter/implementations/LeakyBucketImpl.java
+++ src/main/java/org/systemDesign/rateLimiter/implementations/LeakyBucketImpl.java
@@ -1,4 +1,16 @@
 package org.systemDesign.rateLimiter.implementations;
 
+import org.systemDesign.rateLimiter.algorithms.LeakyBucket;
+
 public class LeakyBucketImpl {
+
+    public static void main(String[] args) {
+        LeakyBucket limiter = new LeakyBucket(3,1);
+
+        for(int i=0;i<10;i++){
+            boolean allowed = limiter.allowRequests();
+            System.out.println("Request "+ i + (allowed? " Allowed ": " Denied"));
+        }
+    }
+
 }
